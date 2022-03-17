a845089af96f1690eedfe30f16cbfb010465f23e
Adam La Morre <alamorre@gmail.com>
2022-03-16 18:53:30 -0700

    Added multi-chat-window

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMultiChatLogic } from "react-chat-engine-advanced";
+import { useMultiChatLogic, ChatWindow } from "react-chat-engine-advanced";
 
 const projectId = "90da2e14-0910-4d15-ad84-c52b5dbc81be";
 const username = "adam";
@@ -14,7 +14,34 @@ function App() {
 
   console.log("chatProps", chatProps);
 
-  return <div></div>;
+  return (
+    <div>
+      <ChatWindow
+        chats={chatProps.chats}
+        messages={chatProps.messages}
+        activeChatId={chatProps.activeChatId}
+        username={chatProps.username}
+        peopleToInvite={chatProps.peopleToInvite}
+        hasMoreChats={chatProps.hasMoreChats}
+        hasMoreMessages={chatProps.hasMoreMessages}
+        isChatListLoading={chatProps.isChatListLoading}
+        isChatFeedLoading={chatProps.isChatFeedLoading}
+        isChatSettingsLoading={chatProps.isChatSettingsLoading}
+        onChatFormSubmit={chatProps.onChatFormSubmit}
+        onChatCardClick={chatProps.onChatCardClick}
+        onChatLoaderShow={chatProps.onChatLoaderShow}
+        onMessageLoaderShow={chatProps.onMessageLoaderShow}
+        onMessageLoaderHide={chatProps.onMessageLoaderHide}
+        onBottomMessageShow={chatProps.onBottomMessageShow}
+        onBottomMessageHide={chatProps.onBottomMessageHide}
+        onMessageFormSubmit={chatProps.onMessageFormSubmit}
+        onInvitePersonClick={chatProps.onInvitePersonClick}
+        onRemovePersonClick={chatProps.onRemovePersonClick}
+        onDeleteChatClick={chatProps.onDeleteChatClick}
+        style={{ height: "100vh" }}
+      />
+    </div>
+  );
 }
 
 export default App;
